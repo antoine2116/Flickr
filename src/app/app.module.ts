b3f70629576d75fc3ccea36d730232db42df6f2c
Antoine <antooinerobin@gmail.com>
2020-12-10 14:53:55 +0100

    ajout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResearchComponent } from './research/research.component';
+import { ResultsComponent } from './results/results.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearchComponent,
+    ResultsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
