fcc4e0aaceb164215dbacc0c64d2fe749f9d4a5f
marienfarge <marien.farge@gmail.com>
2020-12-17 17:54:44 +0100

    debut commentaires

---
+++ src/app/shared/flickrCommentaires.ts
@@ -0,0 +1,13 @@
+export class FlickrCommentaires{
+  id: string;
+  name: string;
+  date: string;
+  content: string;
+
+  constructor(info) {
+    this.id = info.id;
+    this.name = info.name;
+    this.date = info.date;
+    this.content = info.content;
+  }
+}
